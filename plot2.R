94b2aff31596564cdb8f77400221ad7f52f59147
shovitraj <shovitraj@gmail.com>
2020-04-10 11:02:06 -0700

    Plot2

--- plot2.R
+++ plot2.R
@@ -1,10 +1,13 @@
+
 #Coursera Data Science Specialization: Exploratory Data Analysis
 #Shovit Raj Bhari
 #Week: 1
 #Project 1
 #Plot 1
 ################################
+rm(list=ls())
 
+library(data.table)
 
 #load data
 datapath<-file.path("./data/household_power_consumption.txt")
@@ -15,24 +18,26 @@ data <- read.table(datapath, header=TRUE, sep=";", stringsAsFactors=FALSE)
 head(data)
 names(data)
 class(data$Date) #Variable Date is in a character format
-class(dataSubset$Global_active_power) #Variable "Global_active_power" is in a character format
+class(data$Global_active_power) #Variable "Global_active_power" is in a character format
 
 #creating the subset of data from the dates 2007-02-01 and 2007-02-02
 
 dataSubset<- subset(data, Date=="1/2/2007" | Date =="2/2/2007")
 #Date variable is in a character from, we need to convert it into date format with as.Date function
-datetime <- as.Date(dataSubset$Date, format="%d/%m/%Y")
+dataSubset$Date <- as.Date(dataSubset$Date, format="%d/%m/%Y")
 
 names(dataSubset) #optional, I like to check variable names when subset is created
+datetime <- paste(as.Date(dataSubset$Date), dataSubset$Time)
 Date_time <- as.POSIXct(datetime)
 #converting variable "Global_active_power" to numeric
 glo_active_power <- as.numeric(dataSubset$Global_active_power)
-glo_active_power
-Date_time
-dim(glo_active_power)
-dim(Date_time)
+
+
 ## Plot 2
 with(dataSubset, {
         plot(glo_active_power~Date_time, type="l",
              ylab="Global Active Power (kilowatts)", xlab="")
 })
+
+dev.copy(png, file = "plot2.png", width=480, height= 480)  
+dev.off() 
